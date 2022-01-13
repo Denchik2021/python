72626b2003635b7a8fededf72a81b936a4f267c7
Denchik2021 <96486968+Denchik2021@users.noreply.github.com>
2022-01-13 21:20:13 +0500

    Iakupov_Denis_dz3

---
+++ task3_3.py
@@ -0,0 +1,12 @@
+def thesaurus(*names):
+    name_dict = dict()
+    for el in sorted(names):
+        key = el[0]
+        if name_dict.get(key) is None:
+            name_dict[key] = [el]
+        else:
+            name_dict[key].append(el)
+    return name_dict
+
+
+print(thesaurus("Иван", "Мария", "Петр", "Илья"))
